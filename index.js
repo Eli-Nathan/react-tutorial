b200e8c2591496078092e86b6d95134c198a0e10
Elijah Nathan <ely.nathan93@gmail.com>
2018-06-21 22:52:14 +0100

    Begin styling

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Import a library to help create component
 import React from 'react';
-import ReactNative from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create component
-
+const App = () => (
+  <Header headerText={'Albums'} />
+);
 
 // Render it to a device
+AppRegistry.registerComponent('albums', () => App);
